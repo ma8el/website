7fdbad6ad12d286465e1c4bfcd3d6b1516063033
Markus Kurbel <m4rku@pm.me>
2023-03-13 21:45:11 +0100

    Adjust social bar

--- src/components/SocialBar.vue
+++ src/components/SocialBar.vue
@@ -10,18 +10,23 @@
     <div class="social-wrap">
       <ul class="social-bar">
         <li>
-          <a :href="links.metadata.github" target="_blank">
-            <font-awesome-icon class="icon" :icon="['fab', 'github-square']" />
+          <a :href="links.github" target="_blank">
+            <font-awesome-icon class="icon" icon="fa-brands fa-github-square" />
           </a>
         </li>
         <li>
-          <a :href="links.metadata.xing" target="_blank">
+          <a :href="links.blog" target="_blank">
+            <font-awesome-icon class="icon" icon="fa-brands fa-blogger" />
+          </a>
+        </li>
+        <li>
+          <a :href="links.xing" target="_blank">
             <font-awesome-icon class="icon" icon="fa-brands fa-square-xing" />
           </a>
         </li>
         <li>
-          <a :href="links.metadata.linkedin" target="_blank">
-            <font-awesome-icon class="icon" :icon="['fab', 'linkedin']" />
+          <a :href="links.linkedin" target="_blank">
+            <font-awesome-icon class="icon" icon="fa-brands fa-linkedin" />
           </a>
         </li>
       </ul>
@@ -39,7 +44,7 @@
   }
   .icon {
     float: left;
-    font-size: 2.5rem;
+    font-size: 2rem;
     color: black;
   }
 }
