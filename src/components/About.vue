6684322cdfb2a7f5b9b18a1f197eae14b5ea5fcd
Markus Kurbel <m4rku@pm.me>
2023-03-13 23:03:14 +0100

    Get about content from db

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,24 +1,56 @@
 <script lang="ts">
 import Title from '@/components/Title.vue'
 import SocialBar from '@/components/SocialBar.vue'
+import axios from 'axios';
+import type { AxiosInstance } from 'axios';
 
 export default {
 components: {
   Title,
   SocialBar
 },
+
 data () {
     return {
       title: 'About',
-      description: 'This is an about page',
-      links: {
-        metadata: {
-          facebook: 'https://www.facebook.com/andrea.morales.581',
-          linkedin: 'https://www.linkedin.com/in/andrea-morales-581/',
-    },
+      description: 'This is me',
+      firstName: '',
+      lastName: '',
+      mail: '',
+      city: '',
+      languages: '',
+      links: {},
+      about: {'who_am_i': '', 'background': '', 'outside_of_work': ''}
+    }
+},
+mounted() {
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const jwtToken = import.meta.env.VITE_JWT_TOKEN;
+    const axiosInstance: AxiosInstance = axios.create({
+      baseURL: apiUrl,
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    });
+    axiosInstance.get('/socials?select=name,href')
+      .then(response => (this.links = response.data.reduce((obj: { [x: string]: any; }, item: { name: string; href: string; }) =>
+                         {obj[item.name.toLowerCase()] = item.href; return obj;})))
+      .catch(error => console.log(error))
+
+    axiosInstance.get('/about')
+      .then(response => (this.about = response.data[0]))
+      .catch(error => console.log(error))
+
+    axiosInstance.get('/me')
+      .then(response => {
+        const {data: responseData, status: responseStatus} = response;
+        this.firstName = responseData[0].first_name;
+        this.lastName = responseData[0].last_name;
+        this.mail = responseData[0].mail;
+        this.city = responseData[0].city;
+        this.languages = `${responseData[0].languages.join(', ')}`;})
+      .catch(error => console.log(error))
   }
-}
-}
 }
 </script>
 
@@ -32,18 +64,23 @@ data () {
   <v-row no-gutters>
     <v-col cols="2">
     </v-col>
-    <v-col cols="5" class="black about-info pa-8 ma-8">
+    <v-col cols="5" class="about-info pa-8 ma-8">
+       <h1>Who am I?</h1>
        <section>
-         <h1>Who am I?</h1>
-         Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
-         Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+         {{  about.who_am_i  }}
+       </section>
+       <section>
+        {{  about.background  }}
+       </section>
+       <section>
+        {{  about.outside_of_work  }}
        </section>
     </v-col>
-    <v-col cols="2" class="black contact about-info pa-8 ma-8">
-      <h1>John Doe</h1>
-      <li><b>Mail</b>: fooafjdkfj</li>
-      <li><b>City</b>: foo</li>
-      <li><b>Languages</b>: foo</li>
+    <v-col cols="3" class="contact about-info pa-8 ma-8">
+      <h1>{{ firstName }} {{ lastName }}</h1>
+      <li ><b>Mail</b>: <a :href="`mailto:${mail}`">{{  mail }}</a></li>
+      <li><b>City</b>: {{  city }}</li>
+      <li><b>Languages</b>: {{  languages }}</li>
       <SocialBar :links="links" />
     </v-col>
   </v-row>
@@ -54,7 +91,7 @@ data () {
 @import '@/assets/consts.scss';
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 auto 0;
   border-radius: 50%;
   width: 15%;
   height: auto;
@@ -62,27 +99,34 @@ data () {
 
 .about {
   background-color: white;
+  width: 100%;
+}
+
+.about-info {
+  text-align: center;
+  color: map-get($colors, secondary);
 }
 
 .about-info h1 {
   text-align: left;
-  font-size: 2rem;
+  margin-bottom: 15px;
 }
 
 .about-info section {
   text-align: left;
-  font-size: 1rem;
   width: 90%;
 }
 .contact li {
   list-style: none;
   text-align: left;
-  font-size: 1rem;
-  width: 90%;
-  margin-bottom: 10px;
+  margin-bottom: 12px;
 }
-
-.black {
-  color: black;
-}
+.contact a {
+  color: map-get($colors, secondary);
+  text-decoration: none;
+  }
+.contact a:hover {
+  background-color: white;
+  text-decoration: underline;
+  }
 </style>
